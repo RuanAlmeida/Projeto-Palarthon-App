c74509b710e7683943e8adc9c618973d9d301b75
Luís Fernando <munhozfontana@gmail.com>
2018-06-30 12:19:02 -0300

    atualização dos marcadores

--- src/app/selecionar-marcadores/selecionar-marcadores.component.ts
+++ src/app/selecionar-marcadores/selecionar-marcadores.component.ts
@@ -1,8 +1,7 @@
-import { AppSenadoService } from './../selecionar-marcadores/app.senado.service';
-import { AppCamaraService } from './../selecionar-marcadores/app.camara.service';
+
 import { Component, OnInit } from '@angular/core';
-import { AppSenadoService } from './../app.senado.service';
-import { AppCamaraService } from '../app.camara.service';
+import { AppSenadoService } from './app.senado.service';
+import { AppCamaraService } from './app.camara.service';
 
 
 @Component({
@@ -13,7 +12,6 @@ import { AppCamaraService } from '../app.camara.service';
 export class SelecionarMarcadoresComponent implements OnInit {
   camaraDados: any;
   senadoDados: any;
-
   camaraSituacao: any;
   senadoSituacao: any;
   casaSel: any;
@@ -52,40 +50,40 @@ export class SelecionarMarcadoresComponent implements OnInit {
   situacaoAtuais: any[];
 
   casas: [
-    { name: 'Senado'; id: '1' },
-    { name: 'Câmara'; id: '2' },
-    { name: 'Senado e Câmara'; id: '3' }
+    { name: 'Senado', id: '1' },
+    { name: 'Câmara', id: '2' },
+    { name: 'Senado e Câmara', id: '3' }
   ];
 
   constructor(
     private senadoService: AppSenadoService,
     private camaraService: AppCamaraService
-  ) {}
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+
 
   // Requisição HTTP dos dados do senado passando
   getSenado(numero, siglaTipo, ano, autor, tramitacao, codSituacao) {
-    // this.senadoService
-    //   .getPlsSenado(numero, siglaTipo, ano, autor, tramitacao, codSituacao)
-    //   .subscribe(res => this.senadoDados = res);
+    this.senadoService
+      .getPlsSenado(numero, siglaTipo, ano, autor, tramitacao, codSituacao)
+      .subscribe(res => this.senadoDados = res);
   }
 
   // Requisição HTTP dos dados da camara passando
 
   getSenadoSituacoes() {
-    this.senadoService
-      .getSituacoes()
-      .subscribe(
-        res => (this.situacaoAtuais = res.ListaSituacoes.Situacoes.Situacao)
-      );
+    this.senadoService.getSituacoes()
+      .subscribe(res => this.situacaoAtuais = res.ListaSituacoes.Situacoes.Situacao);
   }
 
   // Requisição HTTP dos dados da camara passando siglaTipo, ano, autor, tramitacao, codSituacao e ordenar
-  getCamara(numero, siglaTipo, ano, autor, tramitacao, codSituacao, ordenar) {
-    // this.camaraService
-    //   .getPlsCamara(numero, siglaTipo, ano, autor, tramitacao, codSituacao, ordenar)
-    //   .subscribe(res => this.camaraDados = res);
+  getCamara(numero, siglaTipo, ano, autor, tramitacao, codSituacao) {
+    this.camaraService
+      .getPlsCamara(numero, siglaTipo, ano, autor, tramitacao, codSituacao)
+      .subscribe(res => this.camaraDados = res);
   }
 
   // Requisição HTTP dos dados da camara passando
@@ -105,13 +103,18 @@ export class SelecionarMarcadoresComponent implements OnInit {
   getAllSitacoesSenado() {
     this.senadoService
       .getSituacoes()
-      .subscribe(res => (this.situacaoAtuais = res));
+      .subscribe(
+        res => this.situacaoAtuais = res
+      );
   }
   getAllSitacoesCamara() {
     this.senadoService
       .getSituacoes()
-      .subscribe(res => (this.situacaoAtuais = res));
+      .subscribe(
+        res => this.situacaoAtuais = res
+      );
   }
+
 
   casa(casa) {
     if (casa.id === '1') {
@@ -119,8 +122,8 @@ export class SelecionarMarcadoresComponent implements OnInit {
     } else if (casa.id === '2') {
       this.getAllSitacoesCamara();
     } else {
+
     }
   }
 
 }
-
