621eb6aeeba6b7f2fa62f5d1ee3e7e6e8d61fc52
Lucas Marques Franco <lucas10.mf@gmail.com>
2018-06-30 10:04:29 -0300

    add detalhe

--- src/app/selecionar-marcadores/selecionar-marcadores-detalhe/selecionar-marcadores-detalhe.component.ts
+++ src/app/selecionar-marcadores/selecionar-marcadores-detalhe/selecionar-marcadores-detalhe.component.ts
@@ -1,4 +1,7 @@
+import { AppSenadoService } from './../../app.senado.service';
+import { AppCamaraService } from './../../app.camara.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-selecionar-marcadores-detalhe',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelecionarMarcadoresDetalheComponent implements OnInit {
 
-  constructor() { }
+  params: any;
+  dadosPl: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private appCamaraService: AppCamaraService,
+    private appSenadoService: AppSenadoService
+  ) {
+    this.route.params.subscribe( params => this.params = params);
+}
 
   ngOnInit() {
+    if (this.params.casa === 'camara') {
+      this.appCamaraService.getProposicaoDetalhe(this.params.cod)
+      .subscribe(res => {this.dadosPl = res.dados; console.log(this.dadosPl)});
+    }else if (this.params.casa === 'senado') {
+      this.appSenadoService.getMateriaDetalhe(this.params.cod)
+      .subscribe(res => {this.dadosPl = res.DetalheMateria; console.log(this.dadosPl)});
+    }else {
+
+    }
   }
+
+  
 
 }
